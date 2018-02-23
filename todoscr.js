4cb50481d05985c4218337a92c1bb936646ac2a3
Kenny <claudio.h@ilnet.ch>
2018-02-23 18:13:44 +0100

    html dom

--- todoscr.js
+++ todoscr.js
@@ -1,10 +1,14 @@
 ready(() => {
 
     let todoList = new TodoList();
+    todoList.addTask('asdf1');
+    todoList.addTask('asdf2');
+    todoList.addTask('asdf3');
 
-    let taskInputFields = document.getElementById('todo__list');
+    let list = document.getElementById('todo__list');
     let todoInput = document.querySelectorAll('.todo__add');
 
+    let itemCounter = 0;
 
     todoInput.forEach((element) => {
 
@@ -13,11 +17,52 @@ ready(() => {
         function handleKeyUp(event) {
             if(event.keyCode === 13 && todoInput.value != ""){
 
-                todoList.addTask(element.value);
+                let task = todoList.addTask(element.value);
+                addTaskToList(task);
                 element.value = "";
 
                 console.log(todoList.tasks);
             }
         }
     });
+
+    /**
+     * fügt einen Task der Liste(DOM) hinzu
+     * @param task
+     */
+    let addTaskToList = (task)=>{
+        let newDomItem = createItemDom(task);
+        list.appendChild(newDomItem);
+    };
+
+
+    /**
+     * Baut die initiale Liste auf
+     * @param taskListe
+     */
+    let initTaskList = (taskListe) => {
+        taskListe.forEach(task => {
+            let i = createItemDom(task);
+            list.appendChild(i);
+        })
+    };
+
+    /**
+     * Erstellt ein dom item für einen neuen task
+     * @param task
+     * @returns {HTMLLIElement}
+     */
+    let createItemDom = (task) => {
+        let item = document.createElement('li');
+        item.classList.add('todo__item');
+        item.innerHTML = "<div class='todo__checkbox id-" + itemCounter + "'><input type='checkbox'></div><div class='todo__name id-" + itemCounter + "'>" + task.text + "</div><button class='todo__button id-" + itemCounter + "'>×</button>";
+
+        itemCounter++;
+        return item;
+    };
+
+
+    // Initialisiert die Liste
+    list.innerHTML = '';
+    initTaskList(todoList.tasks);
 });
